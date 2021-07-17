c1f4ce9bbab3df9988828fa97df63d617777e46a
Erwin Bonsma <eriban@users.sourceforge.net>
2021-07-17 09:55:30 +0000

    Add placeholder wasm_bindgens

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm-game-of-life!");
+}
+
 pub mod ca;
