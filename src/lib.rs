da25a7e5ce2ba1e58f5da7fabc22a6fdea8294da
Erwin Bonsma <eriban@users.sourceforge.net>
2021-07-18 12:10:55 +0000

    Remote temp integration bootstrap code

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,5 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
-}
-
 pub mod ca;
 pub mod ga;
