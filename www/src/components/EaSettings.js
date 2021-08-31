eb8efa361cfa5ed5cab9a6752c8c2846a432d672
Erwin Bonsma <eriban@users.sourceforge.net>
2021-08-31 20:56:27 +0000

    Only allow valid values for settings

--- www/src/components/EaSettings.js
+++ www/src/components/EaSettings.js
@@ -5,20 +5,24 @@ import React, { useContext } from 'react';
 
 export const SettingsContext = React.createContext();
 
+function bound(value, min, max) {
+    return Math.max(Math.min(max, value), min);
+}
+
 export function settingsReducer(state, action) {
     console.log("dispatched", state, action);
     switch (action.type) {
         case 'populationSize': return {
-            ...state, populationSize: action.value
+            ...state, populationSize: bound(action.value, 10, 1000)
         };
         case 'recombinationRate': return {
-            ...state, recombinationRate: action.value
+            ...state, recombinationRate: bound(action.value, 0, 1)
         };
         case 'mutationRate': return {
-            ...state, mutationRate: action.value
+            ...state, mutationRate: bound(action.value, 0, 1)
         };
         case 'tournamentSize': return {
-            ...state, tournamentSize: action.value
+            ...state, tournamentSize: bound(action.value, 1, 5)
         };
         case 'elitism': return {
             ...state, elitism: action.value
@@ -43,7 +47,7 @@ export function EaSettings() {
         <Form.Group as={Row} controlId="formPopulationSize">
             <Form.Label column sm={6}>Population size</Form.Label>
             <Col sm={6}>
-                <Form.Control type="number" step="10" min="10" max="1000"
+                <Form.Control type="number" step="10"
                     value={settings.populationSize}
                     onChange={e => dispatch({ type: 'populationSize', value: e.target.value })} />
             </Col>
@@ -51,7 +55,7 @@ export function EaSettings() {
         <Form.Group as={Row} controlId="formRecombinationRate">
             <Form.Label column sm={6}>Recombination rate</Form.Label>
             <Col sm={6}>
-                <Form.Control type="number" step="0.1" min="0" max="1"
+                <Form.Control type="number" step="0.1"
                     value={settings.recombinationRate}
                     onChange={e => dispatch({ type: 'recombinationRate', value: e.target.value })} />
             </Col>
@@ -59,7 +63,7 @@ export function EaSettings() {
         <Form.Group as={Row} controlId="formMutationRate">
             <Form.Label column sm={6}>Mutation rate</Form.Label>
             <Col sm={6}>
-                <Form.Control type="number" step="0.1" min="0" max="1" 
+                <Form.Control type="number" step="0.1"
                     value={settings.mutationRate}
                     onChange={e => dispatch({ type: 'mutationRate', value: e.target.value })} />
             </Col>
@@ -67,7 +71,7 @@ export function EaSettings() {
         <Form.Group as={Row} controlId="formTournamentSize">
             <Form.Label column sm={6}>Tournament size</Form.Label>
             <Col sm={6}>
-                <Form.Control type="number" step="1" min="1" max="5" 
+                <Form.Control type="number" step="1"
                     value={settings.tournamentSize}
                     onChange={e => dispatch({ type: 'tournamentSize', value: e.target.value })} />
             </Col>
