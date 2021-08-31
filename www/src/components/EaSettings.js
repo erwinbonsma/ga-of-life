2f2c80331f345f0d07161e762e68e8bffa27c1ce
Erwin Bonsma <eriban@users.sourceforge.net>
2021-08-31 20:50:43 +0000

    Add more settings

--- www/src/components/EaSettings.js
+++ www/src/components/EaSettings.js
@@ -11,15 +11,31 @@ export function settingsReducer(state, action) {
         case 'populationSize': return {
             ...state, populationSize: action.value
         };
+        case 'recombinationRate': return {
+            ...state, recombinationRate: action.value
+        };
+        case 'mutationRate': return {
+            ...state, mutationRate: action.value
+        };
+        case 'tournamentSize': return {
+            ...state, tournamentSize: action.value
+        };
+        case 'elitism': return {
+            ...state, elitism: action.value
+        }
         default:
             console.error('Unexpected action:', action.type);
     }
 }
 
 export const initialSettings = {
-    populationSize: 120
+    populationSize: 100,
+    recombinationRate: 0.4,
+    mutationRate: 0.9,
+    tournamentSize: 2,
+    elitism: false,
 };
-    
+
 export function EaSettings() {
     const { settings, dispatch } = useContext(SettingsContext);
 
@@ -27,21 +43,41 @@ export function EaSettings() {
         <Form.Group as={Row} controlId="formPopulationSize">
             <Form.Label column sm={6}>Population size</Form.Label>
             <Col sm={6}>
-                <Form.Control type="number" step="10" min="10" max="1000" value={settings.populationSize}
-                    onChange={e => dispatch({ type: 'populationSize', value: e.target.value })}
-                />
+                <Form.Control type="number" step="10" min="10" max="1000"
+                    value={settings.populationSize}
+                    onChange={e => dispatch({ type: 'populationSize', value: e.target.value })} />
             </Col>
         </Form.Group>
         <Form.Group as={Row} controlId="formRecombinationRate">
             <Form.Label column sm={6}>Recombination rate</Form.Label>
             <Col sm={6}>
-                <Form.Control type="number" step="0.1" min="0" max="1" />
+                <Form.Control type="number" step="0.1" min="0" max="1"
+                    value={settings.recombinationRate}
+                    onChange={e => dispatch({ type: 'recombinationRate', value: e.target.value })} />
             </Col>
         </Form.Group>
         <Form.Group as={Row} controlId="formMutationRate">
             <Form.Label column sm={6}>Mutation rate</Form.Label>
             <Col sm={6}>
-                <Form.Control type="number" step="0.1" min="0" max="1" />
+                <Form.Control type="number" step="0.1" min="0" max="1" 
+                    value={settings.mutationRate}
+                    onChange={e => dispatch({ type: 'mutationRate', value: e.target.value })} />
+            </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="formTournamentSize">
+            <Form.Label column sm={6}>Tournament size</Form.Label>
+            <Col sm={6}>
+                <Form.Control type="number" step="1" min="1" max="5" 
+                    value={settings.tournamentSize}
+                    onChange={e => dispatch({ type: 'tournamentSize', value: e.target.value })} />
+            </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="formElitism">
+            <Form.Label column sm={6}>Use elitism</Form.Label>
+            <Col sm={6}>
+                <Form.Check type="checkbox" 
+                    value={settings.elitism}
+                    onClick={e => dispatch({ type: 'elitism', value: e.target.checked })} />
             </Col>
         </Form.Group>
     </Form>
