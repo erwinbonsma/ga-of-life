55e8057bd28cdb508854b8fc6d173023f7863d26
Erwin Bonsma <eriban@users.sourceforge.net>
2021-09-07 19:47:57 +0000

    Fix runtime units in EA statistics

--- www/src/components/EaState.js
+++ www/src/components/EaState.js
@@ -10,32 +10,36 @@ export function EaState() {
     const { eaControl } = useContext(ControlContext);
     const eaState = eaControl?.eaState;
 
-    return (eaState && (
-        <Container>
-            <Row>
-                <Col className="Label" xs={8}>Num. generations</Col>
-                <Col className="NumValue" xs={4}>{eaState.generations}</Col>
-            </Row>
-            <Row>
-                <Col className="Label" xs={8}>Num. evaluations</Col>
-                <Col className="NumValue" xs={4}>{eaState.evaluations}</Col>
-            </Row>
-            <Row>
-                <Col className="Label" xs={8}>Num. CA steps</Col>
-                <Col className="NumValue" xs={4}>{eaState.caSteps}</Col>
-            </Row>
-            <Row>
-                <Col className="Label" xs={8}>Max. fitness</Col>
-                <Col className="NumValue" xs={4}>{eaState.maxFitness}</Col>
-            </Row>
-            <Row>
-                <Col className="Label" xs={8}>Run time [s]</Col>
-                <Col className="NumValue" xs={4}>{eaControl.runTime}</Col>
-            </Row>
-            <Row>
-                <Col className="Label" xs={8}>CA steps [s<sup>-1</sup>]</Col>
-                <Col className="NumValue" xs={4}>{eaControl.runTime > 0 ? Math.round(eaState.caSteps / eaControl.runTime) : "-"}</Col>
-            </Row>
-        </Container>
-    )) || null;
+    if (!eaState) {
+        return null;
+    }
+
+    const runTimeInSeconds = eaControl.runTime / 1000;
+
+    return (<Container>
+        <Row>
+            <Col className="Label" xs={8}>Num. generations</Col>
+            <Col className="NumValue" xs={4}>{eaState.generations}</Col>
+        </Row>
+        <Row>
+            <Col className="Label" xs={8}>Num. evaluations</Col>
+            <Col className="NumValue" xs={4}>{eaState.evaluations}</Col>
+        </Row>
+        <Row>
+            <Col className="Label" xs={8}>Num. CA steps</Col>
+            <Col className="NumValue" xs={4}>{eaState.caSteps}</Col>
+        </Row>
+        <Row>
+            <Col className="Label" xs={8}>Max. fitness</Col>
+            <Col className="NumValue" xs={4}>{eaState.maxFitness}</Col>
+        </Row>
+        <Row>
+            <Col className="Label" xs={8}>Run time [s]</Col>
+            <Col className="NumValue" xs={4}>{Math.round(runTimeInSeconds)}</Col>
+        </Row>
+        <Row>
+            <Col className="Label" xs={8}>CA steps [s<sup>-1</sup>]</Col>
+            <Col className="NumValue" xs={4}>{runTimeInSeconds > 0 ? Math.round(eaState.caSteps / runTimeInSeconds) : "-"}</Col>
+        </Row>
+    </Container>);
 }
