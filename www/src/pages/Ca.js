0928cce60ee89c4201c58a9e368917164f696682
Erwin Bonsma <eriban@users.sourceforge.net>
2021-09-07 20:44:16 +0000

    Avoid errors when reloading CA page

--- www/src/pages/Ca.js
+++ www/src/pages/Ca.js
@@ -1,5 +1,13 @@
 import { CaControl } from '../components/CaControl';
+import { useHistory } from "react-router-dom";
 
 export function Ca({ seed }) {
-    return <CaControl seed={seed} />
+    const history = useHistory();
+
+    if (!seed) {
+        console.info("No seed. Redirecting to main page");
+        history.push("/");
+    }
+
+    return (seed ? <CaControl seed={seed} /> : null);
 }
