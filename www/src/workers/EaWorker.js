f748a906cca3bded6b37482da7f6d79b665b7440
Erwin Bonsma <eriban@users.sourceforge.net>
2021-09-12 10:32:30 +0000

    Simplify EA WASM initialisation

--- www/src/workers/EaWorker.js
+++ www/src/workers/EaWorker.js
@@ -1,11 +1,12 @@
-let ea_settings;
+let wasm;
+let wasm_bg;
+
 let ea;
-let memory;
 
 // A bit of a pain to explictly initialize WASM object this way, but there does not seem a much
 // nicer solution, given that the redux state should be immutable.
 function settings_to_wasm(settings) {
-    ea_settings = ea_settings
+    const ea_settings = new wasm.MyEaSettings()
         .set_border(settings.borderWraps)
         .set_mutation_rate(settings.mutationRate)
         .set_recombination_rate(settings.recombinationRate)
@@ -13,18 +14,20 @@ function settings_to_wasm(settings) {
         .set_tournament_size(settings.tournamentSize)
         .set_elitism(settings.elitism)
 
+    console.log({ settings });
+
     return ea_settings;
 }
 
 export async function init(settings) {
-    if (!ea) {
-        const wasm = await import('ga-of-life');
-        ea_settings = new wasm.MyEaSettings();
-        ea = new wasm.MyEvolutionaryAlgorithm(settings_to_wasm(settings));
-
-        const wasm_bg = await import('ga-of-life/ga_of_life_bg.wasm');
-        memory = wasm_bg.memory;
+    if (!wasm) {
+        wasm = await import('ga-of-life');
     }
+    if (!wasm_bg) {
+        wasm_bg = await import('ga-of-life/ga_of_life_bg.wasm');
+    }
+
+    ea = new wasm.MyEvolutionaryAlgorithm(settings_to_wasm(settings));
 
     return ea;
 };
@@ -46,7 +49,7 @@ export function step() {
         avgFitness: ea.avg_fitness(),
         bestGenotype: ea.best_genotype(),
         bestPhenotype: ea.best_phenotype(),
-        geneDistribution: new Float32Array(memory.buffer, ea.gene_distribution(), ea.genotype_len()),
-        cellDistribution: new Float32Array(memory.buffer, ea.cell_distribution(), ea.phenotype_len()),
+        geneDistribution: new Float32Array(wasm_bg.memory.buffer, ea.gene_distribution(), ea.genotype_len()),
+        cellDistribution: new Float32Array(wasm_bg.memory.buffer, ea.cell_distribution(), ea.phenotype_len()),
     }
 }
